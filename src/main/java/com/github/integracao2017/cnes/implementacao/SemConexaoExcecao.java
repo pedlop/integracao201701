93de32bc629fdc49ee79ff5ead14f01eba9e28cb
Gabriel Oliveira <barbosa.olivera1@gmail.com>
2017-05-15 20:45:07 -0300

    #Commit commit inicial com estrutura de classes do projeto seguindo o diagrama de classes do professor.

--- src/main/java/com/github/integracao2017/cnes/implementacao/SemConexaoExcecao.java
+++ src/main/java/com/github/integracao2017/cnes/implementacao/SemConexaoExcecao.java
@@ -1,7 +1,16 @@
 package com.github.integracao2017.cnes.implementacao;
 
 /**
- * Created by gabriel on 08/05/17.
+ * @author gabriel
+ * @version 1.0.0
+ *
+ * Excecao padrao para ser lancada em caso
+ * de alguma falha ao realizar algum request.
  */
 public class SemConexaoExcecao extends RuntimeException {
+
+    /**Messagem para usuario do component.*/
+    public SemConexaoExcecao(String msg) {
+        super(msg);
+    }
 }
