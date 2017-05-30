10579989ced69f29419672f14c5870d461eccb29
Gabriel Oliveira <barbosa.olivera1@gmail.com>
2017-05-30 02:29:49 -0300

    #Commit correcao na localizacao do arquivo de teste.

--- test/com/github/integracao2017/cnes/implementacao/servicos/EstabelecimentoSaudeServiceParserTest.java
+++ test/com/github/integracao2017/cnes/implementacao/servicos/EstabelecimentoSaudeServiceParserTest.java
@@ -3,6 +3,7 @@ package com.github.integracao2017.cnes.implementacao.servicos;
 import com.github.integracao2017.cnes.cnesinterface.Callback;
 import com.github.integracao2017.cnes.cnesinterface.retorno.Retorno;
 import com.github.integracao2017.cnes.implementacao.servicos.EstabelecimentoSaudeServiceParser;
+import com.sun.org.apache.bcel.internal.util.ClassLoader;
 import org.junit.Test;
 
 import java.nio.file.Files;
@@ -19,17 +20,15 @@ import java.util.function.Consumer;
  */
 public class EstabelecimentoSaudeServiceParserTest {
 
-
     @Test
     public void accept() throws Exception {
         StringBuilder strBuild = new StringBuilder();
         Files.readAllLines(
-                Paths.get(new java.io.File(".").getCanonicalPath() + "/src/main/xmlsTeste/estabelecimento_saude_service.xml")
+                Paths.get(ClassLoader.getSystemResource("estabelecimento_saude_service.xml").toURI())
         ).stream().forEach(strBuild::append);
         Callback callback = (Map<String, Retorno> m) -> {};
         Consumer<String> c = new EstabelecimentoSaudeServiceParser(callback);
         c.accept(strBuild.toString());
         assert(true);
     }
-
 }
