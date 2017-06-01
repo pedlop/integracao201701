9b1271553c2a64969c7bbf0c640ed255e59d575e
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2017-06-01 15:06:07 -0300

    #Commit enum EquipamentoService

---
+++ src/main/java/com/github/integracao2017/cnes/cnesinterface/EquipamentoService.java
@@ -0,0 +1,30 @@
+package com.github.integracao2017.cnes.cnesinterface;
+
+/**
+ * Created by Lucas Vinicios.
+ */
+public enum EquipamentoService {
+
+    CODIGO_EQUIPAMENTO("CODIGO_CNES"),
+    DESCRICAO_EQUIPAMENTO("CODIGO_UNIDADE"),
+    CODIGO_TIPOEQUIPAMENTO("NOME_FANTASIA"),
+    DESCRICAO_TIPOEQUIPAMENTO("NOME_EMPRESARIAL"),
+    DESTINACAO_EQUIPAMENTO("CNPJ"),
+    QUANTIDADE_EQUIPAMENTO("INDENTIFICADOR"),
+    QUANTIDADEUSO_EQUIPAMENTO("LONGINTUDE"),
+    DISPONIBILIDADESUS_EQUIPAMENTO("LATITUDE");
+
+
+    /** Variavel para guardar o valor do campo.*/
+    private String chave;
+
+    /**@return Nome do campo.*/
+    public String getChave() {
+        return chave;
+    }
+
+    /**@param e - Nome do campo.*/
+    EquipamentoService(String e) {
+       chave = e;
+    }
+}
